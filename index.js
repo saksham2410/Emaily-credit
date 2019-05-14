00af417bb97b3af34713a430c02b128f819ab0c9
Saksham <gupta.10@iitj.ac.in>
2019-05-14 21:27:32 +0530

    Added mongoose

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
-require('./services/passport');
+require("./services/passport");
+const keys = require("./config/keys");
+const mongoose = require("mongoose");
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true }, function(error) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Successful connection");
+  }
+});
+
 const app = express();
 
-require('./routes/authRoutes')(app);
-
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
