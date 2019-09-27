9d4531c2ea1ba2f0b5c988333b0884e2a42c1c57
Saksham <gupta.10@iitj.ac.in>
2019-09-28 01:29:04 +0530

    created actions

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header";
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 // const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>;
 const SurveyNew = () => <h2>SurveyNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/surveys" component={Dashboard} />
-          <Route exact path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/surveys" component={Dashboard} />
+            <Route exact path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
 
 //Even if we add something like
 // <Route path="/surveys" component={SurveyNew}
@@ -29,5 +36,6 @@ const App = () => {
 //exact = {True} and exact is same in JSX
 
 //webpack is module loader - takes all javascript files and condense it into one file
+export default connect(null, actions)(App);
 
-export default App;
+//now all the actions components are assigned to App component as props
--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {applyMiddleware, createStore} from 'redux';
-
-
 import App from './components/App';
 import reducers from './reducers'
+const ReduxThunk = require('redux-thunk').default;
 
 
 
 
-const store = createStore( reducers,{},applyMiddleware());
+
+
+const store = createStore( reducers,{},applyMiddleware(ReduxThunk));
 
 ReactDOM.render(<Provider store={store}>
     <App />
@@ -21,4 +22,8 @@ ReactDOM.render(<Provider store={store}>
 //Including App within the provider tag helps to rerender the App component everytime provider tags have changes in the store
 //Reducers are just a way to pass data to the redux
 //Providers tag helps to pass updated data to all the components.
+//Action create initiates change inside the redux side of the application
 //createStore has three arguments - reducers (return value), {} (input), applyMiddleware (some mid stuff)
+
+//React component calls a action creater which produces a action and then sends it to dispatch function
+//Dispatch sends the action to all the different reducers in the store, causing them to instantly recalculate the app state 
